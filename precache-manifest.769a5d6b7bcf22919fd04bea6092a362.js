da9530a2820a1e669ba152b567b720d13ef4244d
prajeshkotian <prajeshkotian@gmail.com>
2020-09-13 15:05:32 +0530

    Updates

---
+++ precache-manifest.769a5d6b7bcf22919fd04bea6092a362.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f370d94d37dfa54e8a35a95b10bde6b9",
+    "url": "/Diagnal-LazyLoading-MovieApp/index.html"
+  },
+  {
+    "revision": "de7c6ab0085a62067071",
+    "url": "/Diagnal-LazyLoading-MovieApp/static/css/3.a130e0f4.chunk.css"
+  },
+  {
+    "revision": "e70767de5990bf6f7588",
+    "url": "/Diagnal-LazyLoading-MovieApp/static/css/main.d7fd9cfa.chunk.css"
+  },
+  {
+    "revision": "5bc4ac67c4e8f8224b2e",
+    "url": "/Diagnal-LazyLoading-MovieApp/static/js/2.dc11e226.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Diagnal-LazyLoading-MovieApp/static/js/2.dc11e226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de7c6ab0085a62067071",
+    "url": "/Diagnal-LazyLoading-MovieApp/static/js/3.d3205a5c.chunk.js"
+  },
+  {
+    "revision": "e70767de5990bf6f7588",
+    "url": "/Diagnal-LazyLoading-MovieApp/static/js/main.09829322.chunk.js"
+  },
+  {
+    "revision": "d20d92018aee1561e44a",
+    "url": "/Diagnal-LazyLoading-MovieApp/static/js/runtime-main.d39bb279.js"
+  }
+]);
